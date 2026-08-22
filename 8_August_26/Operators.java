ca866f2e17241ef96774c2d3623fdb0e399d3676
khushi-596 <dudhalkarkhushi@gmail.com>
2026-08-22 22:56:28 +0530

    Add basic arithmetic operators example

---
+++ 8_August_26/Operators.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Operators {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        System.out.println("Sum = " + (a + b));
+        System.out.println("Difference = " + (a - b));
+        System.out.println("Product = " + (a * b));
+        System.out.println("Quotient = " + ((double)a / b));
+
+        sc.close();
+    }
+}
