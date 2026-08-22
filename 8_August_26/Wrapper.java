3d053d20c5a1ca429c33b5e28c018d933320835e
khushi-596 <dudhalkarkhushi@gmail.com>
2026-08-22 23:06:58 +0530

    Add Java wrapper class example

---
+++ 8_August_26/Wrapper.java
@@ -0,0 +1,24 @@
+public class Wrapper {
+    public static void main(String[] args) {
+        System.out.println("Int Wrapped number min: " + Integer.MIN_VALUE);
+        System.out.println("Int Wrapped number max: " + Integer.MAX_VALUE);
+
+        System.out.println("Double Wrapped number min: " + Double.MIN_VALUE);
+        System.out.println("Double Wrapped number max: " + Double.MAX_VALUE);
+
+        System.out.println("Float Wrapped number min: " + Float.MIN_VALUE);
+        System.out.println("Float Wrapped number max: " + Float.MAX_VALUE);
+
+        System.out.println("Long Wrapped number min: " + Long.MIN_VALUE);
+        System.out.println("Long Wrapped number max: " + Long.MAX_VALUE);   
+
+        System.out.println("Short Wrapped number min: " + Short.MIN_VALUE);
+        System.out.println("Short Wrapped number max: " + Short.MAX_VALUE); 
+
+        System.out.println("Character Wrapped number min: " + Character.MIN_VALUE);
+        System.out.println("Character Wrapped number max: " + Character.MAX_VALUE);
+
+        System.out.println("Boolean Wrapped number min: " + Boolean.FALSE);
+        System.out.println("Boolean Wrapped number max: " + Boolean.TRUE);
+    }
+}
